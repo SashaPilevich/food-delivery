68a1c1ed34e13037d07d99ff305eecfcefd46723
Sasha <pilevichsasha@gmail.com>
2023-06-21 18:18:35 +0300

    [PT-1] add fonts create dark/light appTheme

---
+++ food_delivery/core_ui/lib/src/theme/app_theme.dart
@@ -0,0 +1,80 @@
+import 'package:core_ui/src/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: AppColors.pink,
+      ),
+      primaryColor: AppColors.pink,
+      canvasColor: AppColors.grey,
+      fontFamily: 'RobotoCondensed',
+      appBarTheme: const AppBarTheme(
+        titleTextStyle: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(
+          color: AppColors.pink,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        titleMedium: TextStyle(
+          color: AppColors.pink,
+          fontSize: 17,
+          fontWeight: FontWeight.bold,
+        ),
+        labelMedium: TextStyle(
+          color: AppColors.white,
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: AppColors.orange,
+      ),
+      primaryColor: AppColors.orange,
+      canvasColor: AppColors.black,
+      fontFamily: 'RobotoCondensed',
+      appBarTheme: const AppBarTheme(
+        titleTextStyle: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(
+          color: AppColors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        titleMedium: TextStyle(
+          color: AppColors.white,
+          fontSize: 17,
+          fontWeight: FontWeight.bold,
+        ),
+        labelMedium: TextStyle(
+          color: AppColors.white,
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      cardTheme: const CardTheme(
+        color: Color.fromARGB(255, 75, 71, 71),
+      ),
+      filledButtonTheme: const FilledButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStatePropertyAll(AppColors.white),
+        ),
+      ),
+    );
+  }
+}
--- food_delivery/test/widget_test.dart
+++ food_delivery/test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:food_delivery/app/delivery_food_app.dart';
 
-import 'package:food_delivery/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const DeliveryFoodApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
