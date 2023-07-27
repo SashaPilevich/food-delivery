113f53d5a669b453e5d799baf4f5fe4c07ee82e2
Sasha <pilevichsasha@gmail.com>
2023-07-27 22:43:15 +0300

    [PT-5] fix auth bloc

--- food_delivery/features/auth/lib/src/ui/widgets/text_field.dart
+++ food_delivery/features/auth/lib/src/ui/widgets/text_field.dart
@@ -1,39 +1,39 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class CustomTextField extends StatelessWidget {
-//   final String? Function(String?)? validator;
-//   final TextEditingController textEditingController;
-//   final String hintText;
-//   final bool obscureText;
-//   final Icon icon;
+class CustomTextField extends StatelessWidget {
+  final String? Function(String?)? validator;
+  final TextEditingController textEditingController;
+  final String hintText;
+  final bool obscureText;
+  final Icon icon;
 
-//   const CustomTextField({
-//     required this.validator,
-//     required this.textEditingController,
-//     required this.hintText,
-//     required this.obscureText,
-//     required this.icon,
-//     super.key,
-//   });
+  const CustomTextField({
+    required this.validator,
+    required this.textEditingController,
+    required this.hintText,
+    required this.obscureText,
+    required this.icon,
+    super.key,
+  });
 
-//   @override
-//   Widget build(BuildContext context) {
-//     final ThemeData themeData = Theme.of(context);
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
 
-//     return TextFormField(
-//       obscureText: obscureText,
-//       decoration: InputDecoration(
-//         icon: icon,
-//         iconColor: themeData.primaryColor,
-//         hintText: hintText,
-//         enabledBorder: UnderlineInputBorder(
-//           borderSide: BorderSide(
-//             color: themeData.primaryColor,
-//           ),
-//         ),
-//       ),
-//       validator: validator,
-//       controller: textEditingController,
-//     );
-//   }
-// }
+    return TextFormField(
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        icon: icon,
+        iconColor: themeData.primaryColor,
+        hintText: hintText,
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: themeData.primaryColor,
+          ),
+        ),
+      ),
+      validator: validator,
+      controller: textEditingController,
+    );
+  }
+}
